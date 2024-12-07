1ed785d885042fb4518e4da6b54392159b9a27f8
Lucas <lbometon@hotmail.fr>
2024-12-07 18:48:41 +0100

    Update training.sh

--- training.sh
+++ training.sh
@@ -3,6 +3,14 @@
 # Met à jour pip
 pip install --upgrade pip
 
+
+# Installation d'unsloth avec la configuration qui fonctionne
+pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
+
+# Installation des dépendances supplémentaires sans réinstaller les packages
+pip install --no-deps "xformers<0.0.26" "trl<0.9.0"
+
+
 # Installation de torch et triton d'abord
 pip install torch==2.0.1
 pip install triton==2.0.0
@@ -16,11 +24,6 @@ pip install pandas>=2.2.3
 pip install scikit-learn
 pip install scipy
 
-# Installation d'unsloth avec la configuration qui fonctionne
-pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
-
-# Installation des dépendances supplémentaires sans réinstaller les packages
-pip install --no-deps "xformers<0.0.26" "trl<0.9.0"
 
 # Exécute le script Python
 python training_ollama.py
