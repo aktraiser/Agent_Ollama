82b44ee46fb6b6b0f177e411b60ff3b834a5805e
Lucas <lbometon@hotmail.fr>
2024-12-07 18:29:32 +0100

    Update training.sh

--- training.sh
+++ training.sh
@@ -2,9 +2,19 @@
 # Met à jour pip
 pip install --upgrade pip
 
-# Installe les dépendances nécessaires
+# Installation des dépendances avec des versions spécifiques
+pip install transformers==4.36.2
+pip install trl==0.7.4
+pip install peft==0.7.1
+pip install accelerate==0.21.0
+pip install bitsandbytes==0.40.0
+pip install pandas>=2.2.3
+pip install scikit-learn
+pip install scipy
+pip install xformers==0.0.22
+
+# Installation d'unsloth après les dépendances
 pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
-pip install --no-deps "xformers<0.0.26" "trl<0.9.0" peft accelerate bitsandbytes scikit-learn scipy joblib threadpoolctl
 
 # Exécute le script Python
 python training_ollama.py
