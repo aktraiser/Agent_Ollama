baf483d5392c727385d589e5dde7093a1bf985cc
Lucas <lbometon@hotmail.fr>
2024-12-07 18:40:05 +0100

    Update training.sh

--- training.sh
+++ training.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
+
 # Met à jour pip
 pip install --upgrade pip
 
-# Installation d'unsloth après les dépendances
-pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
-
-# Installation des dépendances avec des versions spécifiques
-pip install triton.common
+# Installation des dépendances de base d'abord
+pip install torch==2.0.1
 pip install transformers==4.36.2
-pip install trl==0.7.4
-pip install peft==0.7.1
 pip install accelerate==0.21.0
 pip install bitsandbytes==0.40.0
 pip install pandas>=2.2.3
 pip install scikit-learn
 pip install scipy
-pip install xformers==0.0.22
 
+# Installation d'unsloth avec la configuration qui fonctionne
+pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
 
+# Installation des dépendances supplémentaires sans réinstaller les packages
+pip install --no-deps "xformers<0.0.26" "trl<0.9.0"
 
 # Exécute le script Python
 python training_ollama.py
