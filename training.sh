cfabdf5b853e65f9fbbe3aac57375af4e9f885c2
Lucas <lbometon@hotmail.fr>
2024-12-08 16:12:03 +0100

    Update training.sh

--- training.sh
+++ training.sh
@@ -8,7 +8,7 @@ pip install --upgrade pip
 pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
 
 # Installation des dépendances supplémentaires sans réinstaller les packages
-pip install --no-deps "xformers<0.0.26" "trl<0.9.0" peft pandas torch accelerate bitsandbytes scikit-learn scipy joblib threadpoolctl
+pip install --no-deps "xformers<0.0.26" "trl<0.9.0" peft pandas torch cmake accelerate bitsandbytes scikit-learn scipy joblib threadpoolctl
 
 # Exécute le script Python
 python training_ollama.py
