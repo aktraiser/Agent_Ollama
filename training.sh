598a1af5b9237c38692a78e3acfdb2f790b2b3f2
Lucas <lbometon@hotmail.fr>
2024-12-08 16:16:08 +0100

    Update training.sh

--- training.sh
+++ training.sh
@@ -10,5 +10,8 @@ pip install "unsloth[colab-new] @ git+https://github.com/unslothai/unsloth.git"
 # Installation des dépendances supplémentaires sans réinstaller les packages
 pip install --no-deps "xformers<0.0.26" "trl<0.9.0" peft pandas torch cmake accelerate bitsandbytes scikit-learn scipy joblib threadpoolctl
 
+# Installation de llama-cpp-python pour la conversion GGUF
+CMAKE_ARGS="-DLLAMA_CUBLAS=on" pip install llama-cpp-python --force-reinstall --upgrade --no-cache-dir
+
 # Exécute le script Python
 python training_ollama.py
