8f9a6ae4ba17b0a5ba357954abdc2259360797d4
Lucas <lbometon@hotmail.fr>
2024-12-08 16:27:08 +0100

    Update training.sh

--- training.sh
+++ training.sh
@@ -8,7 +8,7 @@ pip install --upgrade pip
 !pip uninstall unsloth -y && pip install --upgrade --no-cache-dir --no-deps git+https://github.com/unslothai/unsloth.git
 
 # Installation des dépendances supplémentaires sans réinstaller les packages
-pip install --no-deps "xformers<0.0.26" "trl<0.9.0" peft pandas torch cmake accelerate bitsandbytes scikit-learn scipy joblib threadpoolctl
+pip install --no-deps "xformers<0.0.26" "trl<0.9.0" peft pandas torch cmake libcurl4 accelerate bitsandbytes scikit-learn scipy joblib threadpoolctl
 
 # Exécute le script Python
 python training_ollama.py
